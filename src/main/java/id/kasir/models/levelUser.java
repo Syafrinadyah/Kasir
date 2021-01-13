88090f9ffc488c77751973cc4a6a526ada43de36
novitaa12 <novitakha25@gmail.com>
2021-01-13 13:52:11 +0700

    Menambahkan levelUser.java

---
+++ src/main/java/id/kasir/models/levelUser.java
@@ -0,0 +1,12 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package id.kasir.app;
+
+
+public class levelUser {
+  public static String username;
+  public static String role;  
+}
